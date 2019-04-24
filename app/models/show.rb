0e865762c03aae701bf1c7064c29fd3e697f75e0
Justin Hall <hall.justin.d@gmail.com>
2019-04-24 04:05:51 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -12,6 +12,18 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
+    self.where("rating=?", self.lowest_rating).first
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+    self.where("rating > ?", 5)
+  end
+
+  def self.shows_by_alphabetical_order
   end
 
 end
