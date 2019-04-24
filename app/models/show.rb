fecebf98552c9bda240d38ac9689e58af3ee1acc
Justin Hall <hall.justin.d@gmail.com>
2019-04-24 03:57:30 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,14 @@
 class Show ActiveRecord < ActiveRecord::Base
   def highest_rating
+    self.maximum(:rating)
   end
 
+  def most_popular_show
+    self.order(:rating).first
+  end
+
+  def lowest_rating
+    self.order(:rating)(:desc).first
+  end
 
 end
